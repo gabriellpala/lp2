fb63b7559fcd48dade6bac58f72c1be584b77a53
gabriellpala <73294128+gabriellpala@users.noreply.github.com>
2022-03-25 19:13:49 -0300

    Add files via upload

---
+++ trabalhoGeometria/src/trabalhoGeometria/Retangulo.java
@@ -0,0 +1,40 @@
+package trabalhoGeometria;
+public class Retangulo {
+	// Atributos
+	
+	private double lado, altura;
+	
+	// Métodos
+	public void setLado(double lado) {
+		if (lado > 0) {
+			this.lado = lado;
+		}
+	}
+	public void setAltura(double altura) {
+		if (altura > 0) {
+			this.altura = altura;
+		}
+	}
+	
+	public double calculaArea() {
+		double area = lado * altura;
+		
+		return area; 
+	}
+	
+	public void desenhar() {
+		for (int i = 0; i < altura; i++) {
+			System.out.print(". ");
+			
+			for (int j = 0; j < lado; j++) {
+				if (i == 0 || i + 1 >= altura) {
+					System.out.print(". ");
+				} else {
+					System.out.print("  ");
+				}
+			}
+			
+			System.out.println(".");
+		}
+	}
+}
